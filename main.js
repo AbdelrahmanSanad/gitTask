0bf265d9addf13ddd67040785e075aeb31e4296f
AbdelrahmanSanad <abdelrahmansanad98@gmail.com>
2023-04-20 22:29:43 +0200

    j

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 console.log("hello")
 console.log("try this");
 console.log("solve me plz ❌🤬");
-console.log("")
-=======
-
-console.log("Hussein")
-console.log("esraa")
-console.log("Abdelrahman");
->>>>>>> a3119eea2da1ae5017c84a8fe3e0a82137a29d80
+console.log("🤷‍♂️🤷‍♂️");
+console.log("yuu");
